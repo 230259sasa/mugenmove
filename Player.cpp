a92b0b520bb92197197350fb640284cc40d751f7
秀俊 笹島 <230259@jc-21.jp>
2024-02-29 14:26:55 +0900

    ちょい修正

--- Player.cpp
+++ Player.cpp
@@ -59,10 +59,10 @@ void Player::Update()
 
 	int tx, ty,rx,ry;
 	//ämÇ©íÜêSÇ™äp
-	tx = (int)(XMVectorGetX(posTmp) + 0.6 - 0.5);//0.2
-	ty = (int)(XMVectorGetZ(posTmp) * -1 - 0.5 + 0.5);//0.1
-	rx = (int)(XMVectorGetX(posTmp) + 1.3 - 0.5);//0.8
-	ry = (int)(XMVectorGetZ(posTmp) * - 1 + 0.4 + 0.5);//0.9
+	tx = (int)(XMVectorGetX(posTmp) + 0.1);//0.2
+	ty = (int)(XMVectorGetZ(posTmp) * -1 + 0.1);//0.1
+	rx = (int)(XMVectorGetX(posTmp) + 0.8);//0.8
+	ry = (int)(XMVectorGetZ(posTmp) * - 1 + 0.9);//0.9
 
 	/*Debug::Log("x,z=");
 	Debug::Log(tx);
